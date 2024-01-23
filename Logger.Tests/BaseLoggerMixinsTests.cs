ac18a3e82af0a45a33020e0a919e9449d1f5ed2d
Purplepigs0129 <praythopson@ewu.edu>
2024-01-22 21:30:24 -0800

    Variable name fixes in BaseLoggerMixinsTests.cs

--- Logger.Tests/BaseLoggerMixinsTests.cs
+++ Logger.Tests/BaseLoggerMixinsTests.cs
@@ -121,8 +121,8 @@ public class TestLogger : BaseLogger
 {
     public List<(LogLevel LogLevel, string Message)> LoggedMessages { get; } = new List<(LogLevel, string)>();
 
-    public override void Log(LogLevel logLevel, string message)
+    public override void Log(LogLevel LogLevel, string Message)
     {
-        LoggedMessages.Add((logLevel, message));
+        LoggedMessages.Add((LogLevel, Message));
     }
 }
