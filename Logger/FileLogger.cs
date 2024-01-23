cc62d7b12cedd6226206057300075caa5ee95635
Purplepigs0129 <praythopson@ewu.edu>
2024-01-22 21:26:05 -0800

    added ; to namespace and fixed variable names in FileLogger.cs

--- Logger/FileLogger.cs
+++ Logger/FileLogger.cs
@@ -3,21 +3,20 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
-namespace Logger
+namespace Logger;
+
+public class FileLogger : BaseLogger
 {
-    public class FileLogger : BaseLogger
+    private readonly string _Path;
+    public FileLogger(string filePath)
     {
-        private readonly string _Path;
-        public FileLogger(string filePath)
-        {
-            this._Path = filePath;
-        }
-        public override void Log(LogLevel logLevel, String message)
-        {
-            DateTime DateTime = DateTime.Now;
-            string ClassName = this.ClassName ?? "Wrong or Null name ?";
-            string Message = $"{DateTime} {ClassName} {logLevel} : {message}";
-            File.AppendAllText(_Path, Message + Environment.NewLine);
-        }
+        this._Path = filePath;
+    }
+    public override void Log(LogLevel LogLevel, String Message)
+    {
+        DateTime DateTime = DateTime.Now;
+        string ClassName = this.ClassName ?? "Wrong or Null name ?";
+        string message = $"{DateTime} {ClassName} {LogLevel} : {Message}";
+        File.AppendAllText(_Path, message + Environment.NewLine);
     }
 }
